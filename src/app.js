6724ce475cca3a0ca0e925a0f095f1e38dbb0ab6
JessicaDabennini <jessica.dabennini@gmail.com>
2024-07-30 17:51:20 +0200

    modifiche

---
+++ src/app.js
@@ -0,0 +1,105 @@
+var Prodotto = /** @class */ (function () {
+    function Prodotto(id, tipo, taglia, colore, dispobibile, prezzo) {
+        this.id = id;
+        this.tipo = tipo;
+        this.taglia = taglia;
+        this.colore = colore;
+        this.disponibile = dispobibile;
+        this.prezzo = prezzo;
+    }
+    Prodotto.prototype.assegnaCliente = function (cliente) {
+        if (this.disponibile) {
+            cliente.ordinaProdotto(this);
+            console.log('Prodotto assegnato');
+        }
+        else {
+            console.log('Prodotto non disponibile');
+        }
+    };
+    return Prodotto;
+}());
+var Cliente = /** @class */ (function () {
+    function Cliente(nome, cognome, email, indirizzo, metodoPagamento, saldoConto) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.indirizzo = indirizzo;
+        this.metodoPagamento = metodoPagamento;
+        this.saldoConto = saldoConto;
+    }
+    Cliente.prototype.ordinaProdotto = function (prodotto) {
+        if (prodotto.disponibile && this.saldoConto >= prodotto.prezzo) {
+            console.log('id articolo: ' + prodotto.id + 'tipologia: ' + prodotto.tipo);
+            console.log('Prodotto ordinato');
+        }
+        else if (!prodotto.disponibile) {
+            console.log('id articolo: ' + prodotto.id + ' tipologia: ' + prodotto.tipo);
+            console.log('Prodotto non disponibile');
+        }
+        else if (this.saldoConto < prodotto.prezzo) {
+            console.log('id articolo: ' + prodotto.id + ' tipologia: ' + prodotto.tipo);
+            console.log('Saldo insufficiente');
+        }
+        else {
+            console.log('Qualcosa è andato storto');
+        }
+    };
+    return Cliente;
+}());
+var ProcessoProduzione = /** @class */ (function () {
+    function ProcessoProduzione(nomeProcesso, inProduzione, descrizione, disponibile) {
+        this.inventario = [];
+        this.nomeProcesso = nomeProcesso;
+        this.inProduzione = inProduzione;
+        this.descrizione = descrizione;
+        this.disponibile = disponibile;
+    }
+    ProcessoProduzione.prototype.aggiungiProdotto = function (prodotto) {
+        if (this.nomeProcesso === 'completato') {
+            this.inventario.push(prodotto);
+            this.descrizione = 'il prodotto è pronto per essere acquistato';
+            this.disponibile = true;
+            console.log(this.descrizione);
+        }
+        else {
+            this.nomeProcesso = 'produzione';
+            this.descrizione = 'Il prodotto è in produzione';
+            this.disponibile = false;
+            this.inventario.push(prodotto);
+            console.log(this.descrizione);
+        }
+        ProcessoProduzione.prodottiAggiunti.push(prodotto);
+    };
+    ProcessoProduzione.getProdottiAggiunti = function () {
+        return ProcessoProduzione.prodottiAggiunti.map(function (prodotto) { return prodotto.id && prodotto.tipo; });
+    };
+    ProcessoProduzione.prodottiAggiunti = [];
+    return ProcessoProduzione;
+}());
+var prodotto1 = new Prodotto(1, 'Bikini', 42, 'blu', true, 50);
+console.log(prodotto1);
+var processoProduzione1 = new ProcessoProduzione('completato', [prodotto1], ProcessoProduzione.descrizione, true);
+processoProduzione1.aggiungiProdotto(prodotto1);
+var prodotto2 = new Prodotto(2, 'Pantaloncini', 48, 'rosso', false, 70);
+console.log(prodotto2);
+var processoProduzione2 = new ProcessoProduzione('produzione', [prodotto2], ProcessoProduzione.descrizione, false);
+processoProduzione2.aggiungiProdotto(prodotto2);
+var prodotto3 = new Prodotto(3, 'Pareo', 38, 'verde', true, 90);
+console.log(prodotto3);
+var processoProduzione3 = new ProcessoProduzione('completato', [prodotto3], ProcessoProduzione.descrizione, true);
+processoProduzione3.aggiungiProdotto(prodotto3);
+var prodotto4 = new Prodotto(4, 'Slip', 52, 'arancio', true, 30);
+console.log(prodotto4);
+var processoProduzione4 = new ProcessoProduzione('produzione', [prodotto4], ProcessoProduzione.descrizione, false);
+processoProduzione4.aggiungiProdotto(prodotto4);
+console.log("Inventario Magazzino: " + ProcessoProduzione.getProdottiAggiunti());
+var cliente1 = new Cliente('Jessica', 'Verdi ', 'jessica.dabennini@me.com', 'Roma', 'Carta di credito', 100);
+console.log(cliente1);
+cliente1.ordinaProdotto(prodotto1);
+cliente1.ordinaProdotto(prodotto2);
+cliente1.ordinaProdotto(prodotto3);
+var cliente2 = new Cliente('Roberto', 'Rossi', 'roberto.rossi@me.com', 'Milano', 'PayPal', 50);
+console.log(cliente2);
+cliente2.ordinaProdotto(prodotto2);
+cliente2.ordinaProdotto(prodotto1);
+cliente2.ordinaProdotto(prodotto3);
---
+++ src/app.ts
@@ -0,0 +1,159 @@
+interface Iprodotto {
+    id: number;
+    tipo: string;
+    taglia: number;
+    colore: string;
+    disponibile: boolean;
+
+    assegnaCliente(cliente: Icliente): void;
+}
+
+interface Icliente {
+    nome: string;
+    cognome: string;
+    email: string;
+    metodoPagamento: string;
+
+    ordinaProdotto(prodotto: Iprodotto): void;
+}
+
+interface IprocessoProduzione {
+    nomeProcesso: string;
+    descrizione: string;
+    inProduzione: Iprodotto[];
+
+    aggiungiProdotto(prodotto: Iprodotto): void;
+}
+
+class Prodotto implements Iprodotto {
+    id: number;
+    tipo: string;
+    taglia: number;
+    colore: string;
+    disponibile: boolean;
+    prezzo: number;
+   
+    constructor(id: number, tipo: string, taglia: number, colore: string, dispobibile: boolean, prezzo: number) {
+        this.id = id;
+        this.tipo = tipo;
+        this.taglia = taglia;
+        this.colore = colore;
+        this.disponibile = dispobibile;
+        this.prezzo = prezzo;
+    }
+   
+    assegnaCliente(cliente: Icliente): void {
+        if (this.disponibile) {
+            cliente.ordinaProdotto(this);
+            console.log('Prodotto assegnato');
+        } else {
+            console.log('Prodotto non disponibile');
+        }
+    }
+    }
+
+ class Cliente implements Icliente {
+    nome: string;
+    cognome: string;
+    email: string;  
+    protected indirizzo: string;
+    metodoPagamento: string;
+    private saldoConto: number; 
+
+    constructor(nome: string, cognome: string, email: string, indirizzo: string, metodoPagamento: string, saldoConto: number) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.indirizzo = indirizzo;
+        this.metodoPagamento = metodoPagamento;
+        this.saldoConto = saldoConto;
+    }
+    ordinaProdotto(prodotto: Prodotto): void {
+        if (prodotto.disponibile && this.saldoConto >= prodotto.prezzo) {
+            console.log('id articolo: ' + prodotto.id + 'tipologia: ' + prodotto.tipo);
+            console.log('Prodotto ordinato');
+        } else if (!prodotto.disponibile) {
+            console.log('id articolo: ' + prodotto.id + ' tipologia: ' + prodotto.tipo);
+            console.log('Prodotto non disponibile');
+        } else if (this.saldoConto < prodotto.prezzo) {
+            console.log('id articolo: ' + prodotto.id + ' tipologia: ' + prodotto.tipo);
+            console.log('Saldo insufficiente');
+        } else {
+            console.log('Qualcosa è andato storto');
+        }
+    } 
+}
+
+class ProcessoProduzione implements IprocessoProduzione {
+    nomeProcesso: string;
+    descrizione: string;
+    inProduzione: Prodotto[];  
+    disponibile: boolean;
+    private inventario: Prodotto[] = [];
+    private static prodottiAggiunti: Prodotto[] = [];
+    static descrizione: string;
+  
+    constructor(nomeProcesso: string, inProduzione: Prodotto[], descrizione: string, disponibile: boolean) {
+      this.nomeProcesso = nomeProcesso;
+      this.inProduzione = inProduzione;
+      this.descrizione = descrizione;
+      this.disponibile = disponibile;
+    }
+  
+    aggiungiProdotto(prodotto: Prodotto): void {
+      if (this.nomeProcesso === 'completato') {
+        this.inventario.push(prodotto);            
+        this.descrizione = 'il prodotto è pronto per essere acquistato';
+        this.disponibile = true;
+        console.log(this.descrizione);
+      } else {
+        this.nomeProcesso = 'produzione';
+        this.descrizione = 'Il prodotto è in produzione';
+        this.disponibile = false;
+        this.inventario.push(prodotto);
+        console.log(this.descrizione);
+      }
+      ProcessoProduzione.prodottiAggiunti.push(prodotto);
+    }
+  
+    static getProdottiAggiunti(): any[] {
+        return ProcessoProduzione.prodottiAggiunti.map(prodotto => (prodotto.id));
+      }
+  }
+
+ let prodotto1 = new Prodotto(1, 'Bikini', 42, 'blu', true, 50);
+ console.log(prodotto1);
+ let processoProduzione1 = new ProcessoProduzione('completato', [prodotto1], ProcessoProduzione.descrizione, true);
+ processoProduzione1.aggiungiProdotto(prodotto1);
+
+ let prodotto2 = new Prodotto(2, 'Pantaloncini', 48, 'rosso', false, 70);
+ console.log(prodotto2);
+ let processoProduzione2 = new ProcessoProduzione('produzione', [prodotto2], ProcessoProduzione.descrizione, false);
+ processoProduzione2.aggiungiProdotto(prodotto2);
+
+ let prodotto3 = new Prodotto(3, 'Pareo', 38, 'verde', true, 90);
+ console.log(prodotto3);
+ let processoProduzione3 = new ProcessoProduzione('completato', [prodotto3], ProcessoProduzione.descrizione, true);
+ processoProduzione3.aggiungiProdotto(prodotto3);
+
+ let prodotto4 = new Prodotto(4, 'Slip', 52, 'arancio', true, 30);
+ console.log(prodotto4);
+ let processoProduzione4 = new ProcessoProduzione('produzione', [prodotto4], ProcessoProduzione.descrizione, false);
+ processoProduzione4.aggiungiProdotto(prodotto4);
+
+ console.log("Inventario Magazzino: " + ProcessoProduzione.getProdottiAggiunti());
+
+ let cliente1 = new Cliente('Jessica', 'Verdi ', 'jessica.dabennini@me.com', 'Roma', 'Carta di credito', 100);
+ console.log(cliente1);
+ cliente1.ordinaProdotto(prodotto1);
+ cliente1.ordinaProdotto(prodotto2);
+ cliente1.ordinaProdotto(prodotto3);
+
+ let cliente2 = new Cliente('Roberto', 'Rossi', 'roberto.rossi@me.com', 'Milano', 'PayPal', 50);
+ console.log(cliente2);
+ cliente2.ordinaProdotto(prodotto2);
+ cliente2.ordinaProdotto(prodotto1);
+ cliente2.ordinaProdotto(prodotto3);
+
+
+
